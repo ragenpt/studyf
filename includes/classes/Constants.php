a33ccce08e7a49613c2e6ae7366a4d9dbe7bbccb
George Marchenko <george.marchenko44@gmail.com>
2022-10-19 09:52:41 -0400

    added error messages

---
+++ includes/classes/Constants.php
@@ -0,0 +1,13 @@
+<?php
+class Constants{
+    public static $firstNameCharacters = "Your first name has to be between 2 and 50 characters.";
+    public static $lastNameCharacters = "Your last name has to be between 2 and 50 characters.";
+    public static $usernameCharacters = "Your username has to be between 2 and 50 characters.";
+    public static $usernameTaken = "This username is taken.";
+    public static $emailDoNotMatch = "Emails do not match.";
+    public static $invalidEmail = "Invalid Email.";
+    public static $emailAlreadyInUse = "Email already in use.";
+    public static $passwordsDoNotMatch = "Passwords do not match.";
+    public static $passwordLength = "Your passwords has to be between 5 and 255 characters.";
+    public static $incorrectCredentials = "Your username or password was incorrect.";
+}
