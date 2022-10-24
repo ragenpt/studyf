7c8c69b8974428b47fac17432710eab3cd015597
George Marchenko <george.marchenko44@gmail.com>
2022-10-24 19:27:33 -0400

    added protectTeacherProperty protectStudentProperty redirectIfAuthenticated functions

--- partials/authCheck.php
+++ partials/authCheck.php
@@ -1,4 +1,37 @@
 <?php
-if (!isset($_SESSION['userLoggedIn'])){
-    header('Location: /soen_proj/register.php');
+// for all teacher pages
+// prevents non teachers accessing teacher page
+function protectTeacherProperty(){
+    if (!isset($_SESSION['userLoggedIn'])){
+        header('Location: /soen_proj/register.php');
+    } else {
+        if (!$_SESSION['userIsTeacher']){
+            header('Location: /soen_proj/studentDashboard.php');
+        }
+    }
+}
+
+// for all student pages
+// prevents non student accessing student links
+function protectStudentProperty(){
+    if (!isset($_SESSION['userLoggedIn'])){
+        header('Location: /soen_proj/register.php');
+    } else {
+        if ($_SESSION['userIsTeacher']){
+            header('Location: /soen_proj/teacherDashboard.php');
+        }
+    }
+}
+
+// for register & login
+// if user is authenticated block access to register & login
+function redirectIfAuthenticated(){
+    if (isset($_SESSION['userLoggedIn'])){
+        if ($_SESSION['userIsTeacher']){
+
+            header('Location: /soen_proj/teacherDashboard.php');
+        } else{
+            header('Location: /soen_proj/studentDashboard.php');
+        }
+    }
 }
