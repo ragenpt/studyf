11d513e867b6b377289dc722f0eb0bfdb4d61fbc
George Marchenko <george.marchenko44@gmail.com>
2022-10-24 18:10:47 -0400

    added navbar for authenticated users

--- base.php
+++ base.php
@@ -1,23 +1,37 @@
 <?php 
 require_once 'includes/config.php';
-require_once 'ti/ti.php';
+require_once 'includes/ti/ti.php';
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>StudyForever</title>
         <link rel='stylesheet' type='text/css' href='assets/style/style.css'/>
         <link rel='stylesheet' type='text/css' href='assets/style/base.css'/>
     </head>
     <body>
-        <!-- NAV BAR -->
+        <?php if (!isset($_SESSION['userLoggedIn'])) : ?>
+            <!-- NAV BAR NOT AUTHENTICATED -->
+                <div class='navbar'>
+                    <a href='about.php' class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/about.php' ? 'active' : '')?>'>Grades.easy</a>
+                    <a href='login.php' class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/login.php' ? 'active' : '')?>'>Log In</a>
+                    <a href='register.php' class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/register.php' ? 'active' : '')?>'>Register</a>
+                </div>
+            <!--  NAV BAR END -->
+        <?php else : ?>
+        <!-- NAV BAR AUTHENTICATED AS TEACHER -->
         <div class="navbar">
-            <a href="index.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/index.php' ? 'active' : '')?>'>StudyForever</a>
-            <a href="login.php" class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/login.php' ? 'active' : '')?>'>Log In</a>
-            <a href="register.php" class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/register.php' ? 'active' : '')?>'>Register</a>
+            <a href="dashboard.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/dashboard.php' ? 'active' : '')?>'>Dashboard</a>
+            <a href="about.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/about.php' ? 'active' : '')?>'>Grades.easy</a>
+            <a href="account.php" class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/account.php' ? 'active' : '')?>'>
+                <?php echo $_SESSION['userLoggedIn'] ?></a>
+            <a href="logout.php" class='auth'>Log Out</a>
         </div>
-        <!-- NAV BAR ------ END -->
-
+        <!--  NAV BAR END -->
+        <!-- NAV BAR AUTHENTICATED AS TEACHER -->
+            <!--    add nav bar    -->
+        <!--  NAV BAR END -->
+        <?php endif; ?>
 
         <?php startblock('main') ?>
 
@@ -32,7 +46,7 @@ require_once 'ti/ti.php';
         <!-- FOOTER -->
         <div class="footer">
             <!-- <h5>footer</h5> -->
-        </div>
-        <!-- FOOTER END -->
+<!--        </div>-->
+<!--         FOOTER END-->
     </body>
 </html>
