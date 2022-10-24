091f59930f7d64315ca941657cf798a747effdcf
George Marchenko <george.marchenko44@gmail.com>
2022-10-24 18:11:53 -0400

    new page

---
+++ about.php
@@ -0,0 +1,104 @@
+<?php
+include 'base.php';
+startblock('main');
+?>
+<div class = "header">
+    <ul class="nav-list">
+        <li><a href="#section-member"><span>Team Members</span></a> </li>
+        <li><a href="#section-about"><span>About Us</span></a></li>
+        <li><a href="#user-guide"><span>User Guide</span></a></li>
+    </ul>
+    <h1 class="title">Grade Management <span>system</span></h1>
+    <p class="desc">Managing your Grades.easy peasey!</p>
+</div>
+
+<div id="section-member" class="members-section">
+    <h2>Team Members</h2>
+    <div class="members">
+        <div class="member">
+            <img src="assets/imgs/m11.png" alt="">
+            <h3>Siavash</h3>
+            <p>i am a second year computer science. I love working out, waking in rainy days, eating pizza with coke and watching movies lol!!! </p>
+        </div>
+        <div class="member">
+            <img src="assets/imgs/m12.png" alt="">
+            <h3>Negar Torab Parhiz</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim tortor at auctor urna nunc id. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Malesuada bibendum arcu vitae</p>
+        </div>
+        <div class="member">
+            <img src="assets/imgs/m11.png" alt="">
+            <h3>Cristian Tesa</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim tortor at auctor urna nunc id. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Malesuada bibendum arcu vitae</p>
+        </div>
+        <div class="member">
+            <img src="assets/imgs/m11.png" alt="">
+            <h3>Lord Voldemort</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim tortor at auctor urna nunc id. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Malesuada bibendum arcu vitae</p>
+        </div>
+    </div>
+</div>
+
+<div id="section-about"  class="about">
+    <div class="circle">
+        <h2>About Us</h2>
+        <p>We are a team of 4, cooperating to create a website that helps teachers to enter grades and also helps students to view their grades. This website makes job easy for accessing grades and retrieving all student's information</p>
+    </div>
+</div>
+
+<div id="user-guide" class="user-guide">
+    <h2>User Guide</h2>
+    <div class="steps">
+        <div class="step">
+            <h4>Step 1</h4>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+        <div class="step">
+            <h4>Step 2</h4>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="step">
+            <h4>Step 3</h4>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="step">
+            <h4>Step 4</h4>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="step">
+            <h4>Step 5</h4>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="step">
+            <h4>Step 6</h4>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php
+endblock();
+?>
