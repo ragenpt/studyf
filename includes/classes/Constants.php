e8640bdaebefe54f25851b47ba8a25df80a6d294
George Marchenko <george.marchenko44@gmail.com>
2022-11-08 15:50:41 -0500

    added errors for assessment form

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,5 +1,6 @@
 <?php
 class Constants{
+    // Auth errors
     public static $firstNameCharacters = "Your first name has to be between 2 and 50 characters.";
     public static $lastNameCharacters = "Your last name has to be between 2 and 50 characters.";
     public static $usernameCharacters = "Your username has to be between 2 and 50 characters.";
@@ -10,4 +11,8 @@ class Constants{
     public static $passwordsDoNotMatch = "Passwords do not match.";
     public static $passwordLength = "Your passwords has to be between 5 and 255 characters.";
     public static $incorrectCredentials = "Your username or password was incorrect.";
+
+    // Assessment errors
+    public static $percentageOverflow = "With given weight of the assessment, total weight exceeds 100%, please decrease weight of the assessment.";
+    public static $notEnoughMarks = "You cannot have more questions than marks.";
 }
