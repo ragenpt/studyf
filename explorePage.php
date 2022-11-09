5c270c993a241e3775147ba6f7d06aebed888d0f
George Marchenko <george.marchenko44@gmail.com>
2022-11-09 08:50:13 -0500

    created static html

---
+++ explorePage.php
@@ -0,0 +1,180 @@
+<?php
+include 'partials/base.php';
+include_once 'partials/authCheck.php';
+
+protectStudentProperty();
+startblock('main');
+?>
+<div class="explorePage">
+    <div class="searchBarContainer">
+        <div class="searchBar">
+            <h3>Explore all possible courses here</h3>
+            <form action="" method="POST">
+                <input type="text" placeholder="Enter topic of interest or Course Code">
+            </form>
+        </div>
+    </div>
+    <div class="suggestedCoursesContainer">
+        <h1>Explore Courses Here</h1>
+        <div class="suggestedCourses">
+            <div class="suggestedCourse">
+                <div class="courseTitle">
+                    <h3>Course Name</h3>
+                </div>
+                <div class="courseImage">
+                    <img src="assets/imgs/jsIMG.png" alt="Course Img">
+                </div>
+                <div class="courseTags">
+                    <p>Web</p>
+                    <p>React</p>
+                    <p>JS</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div class="courseDesc">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                        it to make a type specimen book. It has survived not only five centuries, but also
+                        the leap into electronic typesetting, remaining essentially
+                        unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+            <div class="suggestedCourse">
+                <div class="courseTitle">
+                    <h3>Course Name</h3>
+                </div>
+                <div class="courseImage">
+                    <img src="assets/imgs/jsIMG.png" alt="Course Img">
+                </div>
+                <div class="courseTags">
+                    <p>Web</p>
+                    <p>React</p>
+                    <p>JS</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div class="courseDesc">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                        unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+            <div class="suggestedCourse">
+                <div class="courseTitle">
+                    <h3>Course Name</h3>
+                </div>
+                <div class="courseImage">
+                    <img src="assets/imgs/jsIMG.png" alt="Course Img">
+                </div>
+                <div class="courseTags">
+                    <p>Web</p>
+                    <p>React</p>
+                    <p>JS</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div class="courseDesc">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                        it to make a type specimen book. It has survived not only five centuries, but also
+                        the leap into electronic typesetting, remaining essentially
+                        unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+            <div class="suggestedCourse">
+                <div class="courseTitle">
+                    <h3>Course Name</h3>
+                </div>
+                <div class="courseImage">
+                    <img src="assets/imgs/jsIMG.png" alt="Course Img">
+                </div>
+                <div class="courseTags">
+                    <p>Web</p>
+                    <p>React</p>
+                    <p>JS</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div class="courseDesc">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text
+                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+            <div class="suggestedCourse">
+                <div class="courseTitle">
+                    <h3>Course Name</h3>
+                </div>
+                <div class="courseImage">
+                    <img src="assets/imgs/jsIMG.png" alt="Course Img">
+                </div>
+                <div class="courseTags">
+                    <p>Web</p>
+                    <p>React</p>
+                    <p>JS</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div class="courseDesc">
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </p>
+                </div>
+            </div>
+            <div class="suggestedCourse">
+                <div class="courseTitle">
+                    <h3>Course Name</h3>
+                </div>
+                <div class="courseImage">
+                    <img src="assets/imgs/jsIMG.png" alt="Course Img">
+                </div>
+                <div class="courseTags">
+                    <p>Web</p>
+                    <p>React</p>
+                    <p>JS</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div class="courseDesc">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                        it to make a type specimen book. It has survived not only five centuries, but also
+                        PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+            <div class="suggestedCourse">
+                <div class="courseTitle">
+                    <h3>Course Name</h3>
+                </div>
+                <div class="courseImage">
+                    <img src="assets/imgs/jsIMG.png" alt="Course Img">
+                </div>
+                <div class="courseTags">
+                    <p>Web</p>
+                    <p>React</p>
+                    <p>JS</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div class="courseDesc">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+endblock();
+?>
+
