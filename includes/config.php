294369909661b41d586a1cd20ceb3b535d78dbfa
George Marchenko <george.marchenko44@gmail.com>
2022-11-09 08:34:56 -0500

    added error tracking

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1); // show errors
+error_reporting(-1); // of all levels
+date_default_timezone_set('UTC'); // PHP will complain about on this error level
 ob_start(); // Turns on output buffering 
 session_start();
 date_default_timezone_set("America/Toronto");
